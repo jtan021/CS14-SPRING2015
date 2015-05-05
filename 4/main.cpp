2484371e41e8c3df65866ebbaabb6816c478a662
jtan021 <jtan021@ucr.edu>
2015-05-05 18:50:39 +0000

    First Commit

---
+++ 4/main.cpp
@@ -0,0 +1,17 @@
+#include "lab3.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+    if(argc != 2) 
+    {
+        cout << "User: lab3 <Value>\n";
+        exit(-1);
+    }
+    int i = (int)(argv[1][0] - '0');
+    print_preOrder(i);
+    print_postOrder(i);
+    print_inOrder(i);
+    return 0;
+}
